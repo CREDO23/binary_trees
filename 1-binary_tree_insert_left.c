0ceedccf40b3dd91d0c396fd1d67b43d5a0f6777
CREDO23 <thierrybakera12@gmail.com>
2023-08-03 22:20:52 +0200

    feat: add solutions 4-7

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,6 @@
  *                           of another in a binary tree.
  * @parent: A pointer to the node to insert the left-child in.
  * @value: The value to store in the new node.
- *
  * Return: a pointer to the new node.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
